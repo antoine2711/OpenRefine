dab0a0e98afe3ccbeb6cb2859d0ed72dca551841
Antoine Beaubien <antoine@beaubien.qc.ca>
2022-04-23 00:43:10 -0400

    Update index.js
    Remove extra line

--- main/webapp/modules/core/scripts/index.js
+++ main/webapp/modules/core/scripts/index.js
@@ -207,7 +207,6 @@ $(function() {
     .css("width", ($('#right-panel').width() - rightPanelBodyHPaddings) + "px")
     .css("height", ($('#right-panel').height() - rightPanelBodyVPaddings) + "px");
 
-
     for (var i = 0; i < Refine.actionAreas.length; i++) {
       Refine.actionAreas[i].ui.resize();
     }
